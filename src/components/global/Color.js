0840f0670dde15e0a184a576c00068c291bb69d3
andrebeolchi <rm84163@fiap.com.br>
2021-10-25 10:53:55 -0300

    Removendo lightblue da paleta

--- src/components/global/Color.js
+++ src/components/global/Color.js
@@ -5,8 +5,6 @@ export const Color = {
     gray: '#E1E1E1',
     white: '#FFFFFF',
     blue: "#0F42FA",
-    lightblue: "#9BB0FD",
     darkBlue: '#010106',
-    red: "#FF0000", 
-    green: "#00FF00"
+    red: "#FF0000"
 }
